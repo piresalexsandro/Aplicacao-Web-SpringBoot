ce407e190469e49c004c0eaf278db058b77cae15
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-04-19 22:53:04 -0300

    Criando camanda MVC

--- src/main/java/com/eventoapp/EventoappApplication.java
+++ src/main/java/com/eventoapp/EventoappApplication.java
@@ -1,9 +1,11 @@
-package com.eventoapp.eventoapp;
+package com.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.eventoapp.controllers"})
 public class EventoappApplication {
 
 	public static void main(String[] args) {
