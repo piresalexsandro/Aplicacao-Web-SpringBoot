5c2bb55da7dbccb67fbedef89ab0b76d5a3af5b5
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-04-23 21:46:37 -0300

    Realizando buscas no banco de dados MySQL

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventoapp.models.Evento;
 import com.eventoapp.repository.EventoRepository;
 
 @Controller
+//@RequestMapping("/evento")
 public class EventoController {
 	
 	@Autowired
@@ -25,7 +27,19 @@ public class EventoController {
 
 		er.save(evento);
 			
+		//return "redirect:/listaEvento";
 		return "redirect:/cadastrarEvento";
 	}
-
+	
+	@RequestMapping("/eventos")
+	public ModelAndView ListaEventos() {
+		// recebe a lista de eventos
+		ModelAndView mv = new ModelAndView("index"); // <== para onde sera redirecionada
+		//busca lista de eventos no banco de dados
+		Iterable<Evento> eventos = er.findAll();
+		//"eventos" <= que foi definido na index html => ${eventos}*
+		// mostra a lista de eventos na view localhost:8080/eventos
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
 }
